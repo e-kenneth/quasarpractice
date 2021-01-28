9a3caf017200199c14b5d037f43d09bd9b5f178d
Ernest Kenneth <ernestkennethyoungsumarno@gmail.com>
2021-01-28 15:59:18 +0800

    first commit

---
+++ src/pages/ToDoList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+       <q-input v-model="text" label="Standard" />
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+              
+            }
+        },
+        computed: {
+            text() {
+                return this.$store.state.todolist.text;
+                 
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
